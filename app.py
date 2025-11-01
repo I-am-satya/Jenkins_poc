c49899c6769e74c07833f5ebcdde903f5d83d4d4
Ubuntu <Satya-Venkat@Satya-vm1.r1ixl4nx1hre5oc34bjyah1i0a.tx.internal.cloudapp.net>
2025-11-01 12:38:31 +0000

    flask application has been updated

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins!"
+    return "Hello, Satya! Your Flask app is running via Jenkins and Docker."
+
+@app.route('/health')
+def health():
+    return {"status": "UP"}, 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
