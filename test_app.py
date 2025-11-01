b8f4c9e6ec4422de1509c9e81c173e9abc89c328
Ubuntu <Satya-Venkat@Satya-vm1.r1ixl4nx1hre5oc34bjyah1i0a.tx.internal.cloudapp.net>
2025-11-01 12:51:15 +0000

    updated test_app.py

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, Jenkins!"
+    assert response.data == b"Hello, Satya! Your Flask app is running via Jenkins and Docker."
